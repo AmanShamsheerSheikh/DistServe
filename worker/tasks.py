faf5427e4290688d09749dc01cf46df9519b487b
AmanShamsheerSheikh <sheikhaman306@gmail.com>
2026-05-21 17:40:28 +0000

    Feature/Added forgotten changes.

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,15 +1,23 @@
 from worker.celery_app import celery_app
 from db.queries import update_job
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.environ.get("POSTGRES_USER")
+db_password = os.environ.get("POSTGRES_PASSWORD")
+db_name = os.environ.get("POSTGRES_DB")
 
 @celery_app.task
 def run_ml_training_job(job_id: int, job_name: str):
     conn = psycopg2.connect(
-        user='admin',
-        password='password',
+        user=db_user,
+        password=db_password,
         host='localhost',
         port=6432,
-        database='mljobs',
+        database=db_name,
     )
     try:
         update_job(conn, 'STARTED', job_id)
