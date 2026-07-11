3483505c30543b935f01a17cced90a109f24ab47
AmanShamsheerSheikh <sheikhaman306@gmail.com>
2026-07-11 18:13:26 +0000

    -- Added fix and docker file for gpu worker

--- gpu_worker/settings.py
+++ gpu_worker/settings.py
@@ -1,5 +1,5 @@
 from discom.settings import RedisSettings, LLMSettings, ApiSettings
 
-redis_settings = RedisSettings
-llm_settings = LLMSettings
-api_settings = ApiSettings
+redis_settings = RedisSettings()
+llm_settings = LLMSettings()
+api_settings = ApiSettings()
